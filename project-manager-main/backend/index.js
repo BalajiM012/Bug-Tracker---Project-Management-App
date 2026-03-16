62f9afc8e69c94d3700ae2b1a26f5af69211603e
BalajiM012 <113889348+BalajiM012@users.noreply.github.com>
2026-03-16 12:28:15 +0530

    Update index.js

--- project-manager-main/backend/index.js
+++ project-manager-main/backend/index.js
@@ -7,7 +7,6 @@ import morgan from "morgan";
 import routes from "./routes/index.js";
 
 dotenv.config();
-console.log("MONGODB_URI =", process.env.MONGODB_URI);
 
 const app = express();
 
@@ -16,35 +15,30 @@ app.use(
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
+
 app.use(morgan("dev"));
-
-// db connection
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("BD Connected successfully."))
-  .catch((err) => console.log("Failed to connect to DB:", err));
-
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB Connected successfully"))
+  .catch((err) => console.log("Failed to connect DB:", err));
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to TaskHub API",
   });
 });
-// http:localhost:500/api-v1/
+
 app.use("/api-v1", routes);
 
-// error middleware
 app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(500).json({ message: "Internal server error" });
 });
 
-// not found middleware
 app.use((req, res) => {
   res.status(404).json({
     message: "Not found",
@@ -54,5 +48,5 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🔥 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
